089250aaeaabae03fdf93ae2b796558239273899
Tomo Bessho <tbessho2@unl.edu>
2021-03-16 19:17:05 -0500

    small changes for hw3

--- submit_rnn.sh
+++ submit_rnn.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #SBATCH --time=6:00:00          # Maximum run time in hh:mm:ss
 #SBATCH --mem=32000             # Maximum memory required (in megabytes)
-#SBATCH --job-name=rnn  # Job name (to track progress)
+#SBATCH --job-name=HW3  # Job name (to track progress)
 #SBATCH --partition=cse479      # Partition on which to run job
 #SBATCH --gres=gpu:4            # Don't change this, it requests a GPU
 
