416e5bfcfa869ee06440544795cc2a8c69b0696e
Tomo Bessho <tbessho2@unl.edu>
2021-04-02 17:43:58 -0500

    commit

--- code/model.py
+++ code/model.py
@@ -4,82 +4,59 @@ import tensorflow_datasets as tfds  # to load training data
 import matplotlib.pyplot as plt  # to visualize data and draw plots
 from tqdm import tqdm  # to track progress of loops
 from tensorflow.keras.layers import *
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras import Sequential
 
 
-# building the model with the vectorize layer and the embedding layer
-def define_model(EMBEDDING_SIZE, MAX_TOKENS, MAX_SEQ_LEN, DROPOUT_RATE):
+def define_discriminator(in_shape=(32, 32, 3)):
+    discriminator = Sequential()
+    discriminator.add(Conv2D(64, (3, 3), padding="same", input_shape=in_shape))
+    discriminator.add(LeakyReLU(alpha=0.2))
+    discriminator.add(Conv2D(128, (3, 3), strides=(2, 2), padding="same"))
+    discriminator.add(LeakyReLU(alpha=0.2))
+    discriminator.add(Conv2D(128, (3, 3), strides=(2, 2), padding="same"))
+    discriminator.add(LeakyReLU(alpha=0.2))
+    discriminator.add(Conv2D(256, (3, 3), strides=(2, 2), padding="same"))
+    discriminator.add(LeakyReLU(alpha=0.2))
+    discriminator.add(Flatten())
+    discriminator.add(Dropout(0.4))
+    discriminator.add(Dense(1, activation="sigmoid"))
 
-    # adding an embedding layer
-    embedding_layer = tf.keras.layers.Embedding(
-        MAX_TOKENS, EMBEDDING_SIZE, input_length=MAX_SEQ_LEN
+    opt = Adam(lr=0.0002, beta_1=0.5)
+    discriminator.compile(
+        loss="binary_crossentropy", optimizer=opt, metrics=["accuracy"]
     )
-
-    # cells = [tf.keras.layers.LSTMCell(256), tf.keras.layers.LSTMCell(64)]
-    # rnn = tf.keras.layers.RNN(cells)
-
-    # attention_in = tf.keras.layers.LSTM(
-    #     100, return_sequences=True, dropout=DROPOUT_RATE
-    # )(embedding_layer)
-
-    # attention_out = tf.keras.layers.Attention()(attention_in)
-
-    lstm_1 = tf.keras.layers.LSTM(100)
-    dropout = tf.keras.layers.Dropout(DROPOUT_RATE)
-    output_layer = tf.keras.layers.Dense(1, activation="sigmoid")
-
-    model = tf.keras.Sequential([embedding_layer, lstm_1, dropout, output_layer])
-
-    return model
+    discriminator.summary()
+    return discriminator
 
 
-def define_rnn(
-    EMBEDDING_SIZE, MAX_TOKENS, MAX_SEQ_LEN, DROPOUT_RATE, REG_CONSTANT, TYPE_OF_RNN
-):
-    # defining the input layer
-    input_layer = Input(shape=(MAX_SEQ_LEN,))
+def define_generator(latent_dim):
 
-    # adding an embedding layer
-    embedding_layer = tf.keras.layers.Embedding(
-        MAX_TOKENS, EMBEDDING_SIZE, input_length=MAX_SEQ_LEN
-    )
+    # modelled off infoGAN
+    generator = Sequential()
 
-    # output of the embedding layer
-    embedding_layer_out = embedding_layer(input_layer)
+    generator.add(Dense(128 * 8 * 8, activation="relu", input_dim=latent_dim))
+    generator.add(Reshape((8, 8, 128)))
+    generator.add(BatchNormalization(momentum=0.8))
+    generator.add(UpSampling2D())
+    generator.add(Conv2D(128, kernel_size=3, padding="same"))
+    generator.add(Activation("relu"))
+    generator.add(BatchNormalization(momentum=0.8))
+    generator.add(UpSampling2D())
+    generator.add(Conv2D(64, kernel_size=3, padding="same"))
+    generator.add(Activation("relu"))
+    generator.add(BatchNormalization(momentum=0.8))
+    generator.add(Conv2D(3, kernel_size=3, padding="same"))
+    generator.add(Activation("tanh"))
+    generator.summary()
+    return generator
 
-    if TYPE_OF_RNN == "LSTM":
-        # LSMT layer
-        lstm_out = tf.keras.layers.LSTM(
-            100,
-            return_sequences=True,
-            kernel_regularizer=tf.keras.regularizers.l2(REG_CONSTANT),
-            recurrent_regularizer=tf.keras.regularizers.l2(REG_CONSTANT),
-            bias_regularizer=tf.keras.regularizers.l2(REG_CONSTANT),
-        )(embedding_layer_out)
-        lstm_out = tf.keras.layers.Dropout(DROPOUT_RATE)(lstm_out)
-    elif TYPE_OF_RNN == "GRU":
-        # LSMT layer
-        lstm_out = tf.keras.layers.GRU(
-            100,
-            return_sequences=True,
-            kernel_regularizer=tf.keras.regularizers.l2(REG_CONSTANT),
-            recurrent_regularizer=tf.keras.regularizers.l2(REG_CONSTANT),
-            bias_regularizer=tf.keras.regularizers.l2(REG_CONSTANT),
-        )(embedding_layer_out)
-        lstm_out = tf.keras.layers.Dropout(DROPOUT_RATE)(lstm_out)
 
-    # attention layer block
-    dim = int(lstm_out.shape[2])  # getting shape
-    attention_layer = Dense(1, activation="tanh")(lstm_out)
-    attention_layer = Flatten()(attention_layer)
-    attention_layer = Activation("softmax")(attention_layer)
-    attention_layer = RepeatVector(dim)(attention_layer)  # recurring layer
-    attention_layer = Permute([2, 1])(attention_layer)
-    attention_out = tf.keras.layers.concatenate([lstm_out, attention_layer])
-    attention_out = Lambda(
-        lambda xin: tf.keras.backend.sum(xin, axis=-2), output_shape=(dim,),
-    )(attention_out)
-    output = Dense(1, activation="sigmoid")(attention_out)
+def define_gan(generator, discriminator):
+    discriminator.trainable = False
+    gan = Sequential()
+    gan.add(generator)
+    gan.add(discriminator)
 
-    model = tf.keras.Model(inputs=input_layer, outputs=output)
-
-    return model
+    gan.compile(loss="binary_crossentropy", optimizer=Adam())
+    return gan
